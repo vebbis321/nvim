aff268c723e1e61727b81bc6add0e02b29c01c7b
vebbis321 <veb.dan@hotmail.com>
2024-04-19 19:14:22 +0200

    update devdocs

--- lua/thel/plugins/dev-docs.lua
+++ lua/thel/plugins/dev-docs.lua
@@ -10,6 +10,12 @@ return {
 	},
 	opts = {
 		dir_path = vim.fn.stdpath("data") .. "/devdocs", -- installation directory
+		float_win = { -- passed to nvim_open_win(), see :h api-floatwin
+			relative = "editor",
+			height = 100,
+			width = 150,
+			border = "rounded",
+		},
 		wrap = true, -- text wrap, only applies to floating window
 	},
 }
