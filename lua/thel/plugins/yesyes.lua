bdb70e8669b4972ac5638cbbea4d877a3686f6f3
vebbis321 <veb.dan@hotmail.com>
2024-04-22 21:55:13 +0200

    added checking checkbox

--- lua/thel/plugins/yesyes.lua
+++ lua/thel/plugins/yesyes.lua
@@ -2,4 +2,11 @@ return {
 	"nvim-lua/plenary.nvim",
 	"christoomey/vim-tmux-navigator",
 	"NvChad/nvim-colorizer.lua",
+	{
+		"opdavies/toggle-checkbox.nvim",
+		config = function()
+			local keymap = vim.keymap
+			keymap.set("n", "<C-Space>", ":lua require('toggle-checkbox').toggle()<CR>j0")
+		end,
+	},
 }
