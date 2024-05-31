71143836d8c8b618e2cc79a8ee1b07b2feaf1798
vebbis321 <veb.dan@hotmail.com>
2024-05-31 14:39:49 +0200

    updated paths and stuff

--- lua/thel/lazy/vimwiki.lua
+++ lua/thel/lazy/vimwiki.lua
@@ -33,7 +33,7 @@ return {
 			[".mdown"] = "markdown",
 		}
 		vim.g.vimwiki_filetypes = { "markdown" }
-		vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
+		-- vim.g.vimwiki_global_ext = 0 -- don't treat all md files as vimwiki (0)
 		-- vim.g.vimwiki_folding = "list"
 		vim.g.vimwiki_hl_headers = 1 -- use alternating colours for different heading levels
 		vim.g.vimwiki_markdown_link_ext = 1 -- add markdown file extension when generating links
