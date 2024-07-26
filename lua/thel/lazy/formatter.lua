598380f2833f13d50fa3eca1ae7759a3737941fc
vebbis321 <veb.dan@hotmail.com>
2024-07-26 17:50:46 +0200

    fix formatting for python

--- lua/thel/lazy/formatter.lua
+++ lua/thel/lazy/formatter.lua
@@ -7,7 +7,7 @@ return {
 		conform.setup({
 			formatters_by_ft = {
 				c = { "clang-format" },
-				python = { "isort", "ruff" },
+				python = { "ruff_format", "ruff_orgaize_imports" },
 				javascript = { "prettier" },
 				typescript = { "prettier" },
 				html = { "prettier" },
@@ -19,27 +19,12 @@ return {
 			},
 			format_on_save = {
 				lsp_fallback = true,
-				async = false,
 				timeouts_ms = 500,
 			},
-		})
-
-		vim.api.nvim_create_user_command("FormatDisable", function(args)
-			if args.bang then
-				-- FormatDisable! will disable formatting just for this buffer
-				vim.b.disable_autoformat = true
-			else
-				vim.g.disable_autoformat = true
-			end
-		end, {
-			desc = "Disable autoformat-on-save",
-			bang = true,
-		})
-		vim.api.nvim_create_user_command("FormatEnable", function()
-			vim.b.disable_autoformat = false
-			vim.g.disable_autoformat = false
-		end, {
-			desc = "Re-enable autoformat-on-save",
+			format_after_save = {
+				lsp_format = "fallback",
+			},
+			notify_on_error = true,
 		})
 
 		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
