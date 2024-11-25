b5bd58d4b850105066159701359eb0df31df8d83
vebbis321 <veb.dan@hotmail.com>
2024-11-25 18:39:19 +0100

    removed sniprun

--- lua/thel/remap.lua
+++ lua/thel/remap.lua
@@ -35,8 +35,7 @@ keymap.set("n", "N", "Nzzzv")
 keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
--- greatest remap ever
--- foo bar
+-- paste over
 keymap.set("x", "<leader>p", [["_dP]])
 
 -- yank into buffer
