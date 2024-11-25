a5fc626d908ab63be258d80cab568e850d913c4b
vebbis321 <veb.dan@hotmail.com>
2024-11-25 13:02:24 +0100

    fixed colors and pane switching

--- lua/thel/lazy/yesyes.lua
+++ lua/thel/lazy/yesyes.lua
@@ -1,5 +1,26 @@
 return {
+	-- most plugins need this
 	"nvim-lua/plenary.nvim",
+
+	-- tmux nav
 	"christoomey/vim-tmux-navigator",
+	cmd = {
+		"TmuxNavigateLeft",
+		"TmuxNavigateDown",
+		"TmuxNavigateUp",
+		"TmuxNavigateRight",
+		"TmuxNavigatePrevious",
+	},
+	keys = {
+		{ "<m-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+		{ "<m-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+		{ "<m-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+		{ "<m-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+	},
+
+	-- colors to hex/rgb
 	"NvChad/nvim-colorizer.lua",
+	event = "BufReadPre",
+	opts = { -- set to setup table
+	},
 }
